8f411725e0f4b01a0d5e9b0b6a690ad2b738b40c
Luxinatux <94648283+luxinatux@users.noreply.github.com>
2021-12-01 15:29:08 -0800

    updates to motor class

--- src/Motor.h
+++ src/Motor.h
@@ -9,13 +9,18 @@ class Motor
 		uint16_t FWD_SIG; // The value of the max forward throttle signal, 1.9 ms
 		uint16_t REV_SIG; // The value of the max reverse throttle signal, 1.1 ms
 		uint16_t STOP_SIG; // The value of the stop signal, 1.5 ms
+        byte SERVO_PIN; // Pin that we will be sending PWm Through
 		uint32_t* ESC_TMRCH_CCR; // The compare/capture register at which the timer will be set
 		uint32_t AUTO_RLD; // The value at which the timer will overflow
 		float PER; // Period of the timer
 		float THROTTLE_SIG; // The throttle signal passed by the MCU to the ESC
 
     public:
-        
+
+        Motor(byte in_pin, uint16_t fwd_sig, uint16_t Reverse, uint16_t stop); //initializing the motor
+        void updateMotor(uint16_t throttle);
+
+
 
 
 };
