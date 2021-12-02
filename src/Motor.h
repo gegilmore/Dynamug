8acf615a04a378af8035702e2563e007fa6a4857
Luxinatux <94648283+luxinatux@users.noreply.github.com>
2021-12-01 23:06:24 -0800

    updated functionality

--- src/Motor.h
+++ src/Motor.h
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-
 class Motor
 {
     protected:
@@ -10,16 +9,17 @@ class Motor
 		uint16_t REV_SIG; // The value of the max reverse throttle signal, 1.1 ms
 		uint16_t STOP_SIG; // The value of the stop signal, 1.5 ms
         byte SERVO_PIN; // Pin that we will be sending PWm Through
-		Servo SERVO;
-		uint32_t* ESC_TMRCH_CCR; // The compare/capture register at which the timer will be set
-		uint32_t AUTO_RLD; // The value at which the timer will overflow
-		float PER; // Period of the timer
-		float THROTTLE_SIG; // The throttle signal passed by the MCU to the ESC
+		Servo sir;
+		//uint32_t* ESC_TMRCH_CCR; // The compare/capture register at which the timer will be set
+		//uint32_t AUTO_RLD; // The value at which the timer will overflow
+		//float PER; // Period of the timer
+		//float THROTTLE_SIG; // The throttle signal passed by the MCU to the ESC
 
     public:
 
-        Motor(byte in_pin, uint16_t fwd_sig, uint16_t Reverse, uint16_t stop); //initializing the motor
-        void updateMotor(uint16_t throttle);
+		Motor(void);
+        void initialize(byte in_pin, uint16_t fwd_sig, uint16_t Reverse, uint16_t stop); //initializing the motor
+        void updateMotor(float throttle);
 
 
 
