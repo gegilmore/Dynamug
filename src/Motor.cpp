6724eedef1d193d6e2fe4d259eba133fb7b540ed
Luxinatux <94648283+luxinatux@users.noreply.github.com>
2021-12-01 23:06:00 -0800

    updated functionality

--- src/Motor.cpp
+++ src/Motor.cpp
@@ -2,32 +2,30 @@
 #include <Motor.h>
 #include <Servo.h>
 
+Motor::Motor(void)
+{
+	Servo sir;
+	// sir.writeMicroseconds(1500);
+	// delay(7000);
+}
 
-
-Motor::Motor(byte in_pin, uint16_t fwd_sig, uint16_t Reverse, uint16_t stop)
+void Motor::initialize(byte in_pin, uint16_t fwd_sig, uint16_t Reverse, uint16_t stop)
  {
-    Servo SERVO;
-    SERVO.attach(in_pin);
+	sir.attach(in_pin);
+	sir.writeMicroseconds(1500);
+	delay(7000);	
     SERVO_PIN = in_pin;
     FWD_SIG = fwd_sig;
     REV_SIG = Reverse;
     STOP_SIG = stop;
 
-	
-
-
 }
 
-void Motor::updateMotor(uint16_t throttle)
+void Motor::updateMotor(float throttle)
 {
 	uint16_t finalmicro = 0;
-    SERVO.attach(SERVO_PIN);
-
-	if( throttle > -100 || throttle < 100 ) 
-    {
- 			printf("Within range");
- 	}	
-    else if( throttle < -100 ) 
+	
+    if( throttle < -100 ) 
     {
  			throttle = -100;
 			finalmicro = REV_SIG;
@@ -40,12 +38,14 @@ void Motor::updateMotor(uint16_t throttle)
 	else if( throttle > 0) 
     {
 			
-			finalmicro = (FWD_SIG - STOP_SIG)*(throttle/100) + STOP_SIG);
+			finalmicro = ((FWD_SIG - STOP_SIG)*(throttle/100) + STOP_SIG);
+			//finalmicro = 1600;
  	}
 	else if( throttle < 0) 
     {
 			
 			finalmicro = (( STOP_SIG - REV_SIG)*(throttle/100) + REV_SIG);
+			
  	}
 	 else if( throttle = 0) 
     {
@@ -59,8 +59,8 @@ void Motor::updateMotor(uint16_t throttle)
 
  	//printf("The duty cycle is: %d\n", throttle_Sig);
 
-	SERVO.writeMicroseconds(finalmicro);
-	
+	sir.writeMicroseconds(finalmicro);
+	Serial.print(finalmicro);
 	//esc->THROTTLE_SIG = throttle_Sig; //example line of code to set throttle
 	// -100 corresponds to 1100 and 100 corresponds to 1900
 	// I need to scale the values from -100 and 100 to the range of 1100 to 1900
