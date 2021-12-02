77e1c94544a633e86e354d250420759bcdf81889
Luxinatux <94648283+luxinatux@users.noreply.github.com>
2021-12-01 20:22:30 -0800

    add this

--- src/Motor.h
+++ src/Motor.h
@@ -10,6 +10,7 @@ class Motor
 		uint16_t REV_SIG; // The value of the max reverse throttle signal, 1.1 ms
 		uint16_t STOP_SIG; // The value of the stop signal, 1.5 ms
         byte SERVO_PIN; // Pin that we will be sending PWm Through
+		Servo SERVO;
 		uint32_t* ESC_TMRCH_CCR; // The compare/capture register at which the timer will be set
 		uint32_t AUTO_RLD; // The value at which the timer will overflow
 		float PER; // Period of the timer
